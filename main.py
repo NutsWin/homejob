f4362c2071daaab5a9f84237b2dfe7b5137f5b70
NutsWin <nutswinjob@yandex.ru>
2024-06-16 22:45:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+str = "My name is Vasya"
+a = len(str)
+print(a)
+
+
+
+first = 10
+second = 10
+summa = first + second
+diff = first - second
+print(summa)
+print (diff)
+
+firstt = 2
+secondd = 4
+third = 6
+mean = (firstt + secondd + third)/2
+print(mean)
+
+
+first_string = 'Вторник'
+second_string = 'Понедельник, '
+print(second_string + first_string)
+
+
+a1 = 2
+b1 = 4
+c1 = 6
+f = (((a1 * b1) + (a1 * c1))**3)/2
+print(f)
